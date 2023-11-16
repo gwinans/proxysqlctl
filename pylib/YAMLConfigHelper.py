8800fae497a730e4c4206f0a08fc7f253b10d5e1
Geoff W <gwinspub@gmail.com>
2023-11-15 22:40:19 -0700

    Just dickin' around.

--- pylib/YAMLConfigHelper.py
+++ pylib/YAMLConfigHelper.py
@@ -4,22 +4,24 @@ import yaml
 
 class YAMLConfigHelper:
     def __init__(self):
-        # self.filename = os.path.expanduser(os.path.join("~", ".config", "proxysqlctl", "proxysqlctl.yaml")),
         self.filename = os.path.expanduser(
-            os.path.join("~", "repos", "proxysql", "config", "proxysqlctl.yaml")
+            os.path.join("~", "repos", "proxysqlctl", "config", "proxysqlctl.yaml")
         )
+        # self.filename = os.path.expanduser(os.path.join("~", ".config", "proxysqlctl", "proxysqlctl.yaml"))
         self.data = None
 
+    def load(self):
         try:
             with open(self.filename, "r") as f:
-                self.data = yaml.safe_load(f).get("proxysqlctl")
+                self.data = yaml.safe_load(f)
         except FileNotFoundError:
             return None
 
-        return data
+        return self.data
 
     def get_instance(self, name):
         return self.data["instances"][f"{name}"]
 
     def get_default_instance(self):
-        return self.data["default_instance"]
+        instance = self.data["default_instance"]
+        return self.data["instances"][f"{instance}"]
