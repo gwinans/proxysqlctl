4cd834305ef346b192dd7d06be6fb05b6c6e1059
Geoff W <gwinspub@gmail.com>
2023-11-16 21:59:24 -0700

    Trying to start off by following standards.

--- proxysqlctl.py
+++ proxysqlctl.py
@@ -10,9 +10,10 @@ def main():
     # Load the config file
     pxcfg = pxyml.load()
 
-    default_instance = pxcfg["instances"][pxcfg["default_instance"]]
+    # default_instance = pxcfg["instances"][pxcfg["default_instance"]]
+    # print(f"Default instance: {default_instance}")
 
-    print(f"Default instance: {default_instance}")
+    print(type(pxcfg))
 
 
 if __name__ == "__main__":
--- pylib/YAMLConfigHelper.py
+++ pylib/YAMLConfigHelper.py
@@ -1,36 +1,50 @@
 import os
 import yaml
 import shutil
+import platform
 from rich import print
 
 
 class YAMLConfigHelper:
     def __init__(self):
-        if os.getenv("XDG_CONFIG_HOME") is not None:
-            self.filename = os.path.join(
-                os.getenv("XDG_CONFIG_HOME"), "proxysqlctl", "proxysqlctl.yaml"
+        self.data = None
+        self.xdg_config_home = None
+
+        # Get the platform
+        self.platform = platform.system()
+
+        if "windows" in self.platform.lower():
+            self.full_config_path = os.path.join(
+                os.getenv("LOCALAPPDATA"), "proxysqlctl", "proxysqlctl.yaml"
             )
         else:
-            self.filename = os.path.expanduser(
-                os.path.join("~", ".config", "proxysqlctl", "proxysqlctl.yaml")
-            )
-        self.data = None
+            if os.getenv("XDG_CONFIG_HOME") is None:
+                self.xdg_config_home = os.path.expanduser(os.path.join("~", ".config"))
+            else:
+                self.xdg_config_home = os.path.expanduser(
+                    os.path.join("~", ".config", "proxysqlctl", "proxysqlctl.yaml")
+                )
+
+        self.full_config_path = os.path.join(
+            self.xdg_config_home, "proxysqlctl", "proxysqlctl.yaml"
+        )
 
     def install(self):
         cfgsrc = os.path.join("config", "proxysqlctl.yaml")
-        cfgdst = self.filename
+        cfgdst = self.full_config_path
 
-        if not os.path.exists(self.filename):
-            print("Installing config file")
-            os.makedirs(os.path.dirname(self.filename), exist_ok=True)
-            shutil.copyfile(cfgsrc, cfgdst)
+        if not os.path.exists(self.full_config_path):
+            print("This appears to be the first time you've run proxysqlctl.")
+            print("  Creating config file.")
+            os.makedirs(os.path.dirname(self.full_config_path), exist_ok=True)
+            shutil.copyfile(cfgsrc, self.full_config_path)
             print(f"Config file installed to:\n   {cfgdst}\n")
             print("Please edit the config file and re-run this command")
             exit(1)
 
     def load(self):
         try:
-            with open(self.filename, "r") as f:
+            with open(self.full_config_path, "r") as f:
                 self.data = yaml.safe_load(f)
         except FileNotFoundError:
             return None
